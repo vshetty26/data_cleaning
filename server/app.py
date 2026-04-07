4f03cab4cd83eaa882b46aa05eaeb83b68944b2a
vshetty26 <2024.vamshis@isu.ac.in>
2026-04-07 11:49:15 +0530

    fix main entrypoint for validator

--- server/app.py
+++ server/app.py
@@ -309,3 +309,11 @@ function copyDirty() {
 </script>
 </body>
 </html>"""
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
